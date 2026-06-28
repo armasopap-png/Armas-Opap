3ab45cf1c2e978484b005bc3dc85acd8b2cdc6ee
armasopap-png <armasopap@gmail.com>
2026-06-29 05:42:07 +0800

    update in ofw

--- includes/db.php
+++ includes/db.php
@@ -4,10 +4,10 @@
  * PDO MySQL connection with error handling
  */
 
-$host = 'sql105.infinityfree.com';
-$dbname = 'if0_42292012_armas';
-$username = 'if0_42292012';
-$password = 'eJs5LzyvQkeqe';
+$host = 'localhost';
+$dbname = 'armas';
+$username = 'root';
+$password = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
