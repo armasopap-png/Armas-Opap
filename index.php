50df0daeca327cb948d69ca8060dad35357d47c0
Reanan Banta <reananbanta@gmail.com>
2026-07-03 05:12:23 +0800

    Fixed index php

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 <?php
+
 /**
  * ARMAS Index - Redirect to landing page
  */
 header('Location: /armas/pages/landing.php');
 exit;
-=======
-<?php
-/**
- * ARMAS Index - Redirect to landing page
- */
-header('Location: /armas/pages/landing.php');
-exit;
->>>>>>> 2325fbda566db981d819227751502fcbf1dc796e
